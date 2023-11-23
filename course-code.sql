2819c4975ae9fd88e6f76ffa9580212414e9e46f
matilda002 <matilda0817@hotmail.com>
2023-11-23 13:23:33 +0100

    changed course code, merged code to one file

--- course-code.sql
+++ course-code.sql
@@ -1,7 +1,26 @@
-CREATE TABLE course (
-  CourseName VARCHAR(20) NOT NULL,
-  ProfessorID INT(11) NOT NULL,
-  Credits DECIMAL(2,1) NOT NULL,
-  DepartmentID INT(11) NOT NULL,
-  Nr_of_Students INT(3) NOT NULL
+CREATE TABLE 'course' (
+  'Course_Name' VARCHAR(20) NOT NULL AUTO_INCREMENT,
+  'Professor_ID' INT(11),
+  'Credits' INT(2) NOT NULL,
+  'Department_ID' INT(11),
+  'Nr_of_Students' INT(3),
+  PRIMARY KEY (Course_Name)
 );
+
+-- Inserting courses and theit credits into the table
+INSERT INTO course (CourseName, credits)
+VALUES
+    ('Biology', 60),
+    ('Chemistry', 70),
+    ('Physics', 50),
+    ('English', 60),
+    ('German', 40),
+    ('Swedish', 35),
+    ('History', 40),
+    ('Math', 80),
+    ('Art', 30),
+    ('Sociology', 20),
+    ('Film Studies', 45),
+    ('Creative writing', 15),
+    ('Photography', 25);
+    
