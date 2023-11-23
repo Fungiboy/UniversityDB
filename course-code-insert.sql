ced89f170f1d1867bdfd4bee18b1534f081d82ac
matilda002 <matilda0817@hotmail.com>
2023-11-23 11:22:17 +0100

    inserting course name and their credits

--- course-code-insert.sql
+++ course-code-insert.sql
@@ -1,19 +1,23 @@
--- Inserting courses into the table
-INSERT INTO course (CourseName, ProfessorID, credits, DepartemntID, Nr_of_Students)
-VALUES (
-    ('Biology', 1, 60, NULL, NULL),
-    ('Chemistry', 1, 70, NULL, NULL),
-    ('Physics', 1, 50, NULL, NULL),
-    ('English', 2, 60, NULL, NULL),
-    ('PE', 3, 40, NULL, NULL),
-    ('Social studies', 4, 35, NULL, NULL),
-    ('History', 4, 40, NULL, NULL),
-    ('Math', 5, 80, NULL, NULL),
-    ('Art', 6, 30, NULL, NULL),
-    ('Home economics', 7, 60, NULL, NULL),
-    ('Sociology', 8, 20, NULL, NULL),
-    ('Film Studies', 9, 45, NULL, NULL),
-    ('Creative writing', 9, 15, NULL, NULL),
-    ('Programming C#', 10, 35, NULL, NULL),
-    ('Photography', 11, 25, NULL, NULL)
-);
+-- Inserting courses and theit credits into the table
+INSERT INTO course (CourseName, credits)
+VALUES
+    ('Biology', 60),
+    ('Chemistry', 70),
+    ('Physics', 50),
+    ('English', 60),
+    ('German', 40),
+    ('Swedish', 35),
+    ('History', 40),
+    ('Math', 80),
+    ('Art', 30),
+    ('Sociology', 20),
+    ('Film Studies', 45),
+    ('Creative writing', 15),
+    ('Photography', 25);
+
+
+
+
+
+
+    
