f3538bcfc19a3fd4166f33dde36bb2bbc657d4e4
matilda002 <matilda0817@hotmail.com>
2023-11-27 16:45:36 +0100

    they exists in univesity-2.0.sql

--- DepartmentHead.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `departmentHead`(
-    HeadProfessorID int(11) NOT NULL,
-    ProfessorID int(11) NOT NULL,
-    DepartmentID int(11) NOT NULL,
-    CourseName varchar(30) NOT NULL
-);
--- Departments.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Departments (
-    DepartemntID int(11) NOT NULL,
-    DepartmentName varchar(31) NOT NULL,
-    ProfessorID int(11) NOT NULL,
-    NumberOfCourses int(11) NOT NULL,
-    HeadProfessorID int(11) NOT NULL
-);
