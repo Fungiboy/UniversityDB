8f90ea89d952cf9af62e89c2bf94c0ed4b28217e
matilda002 <matilda0817@hotmail.com>
2023-11-27 16:23:59 +0100

    re-added a trigger

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,11 @@
+-- Updates the students enrolment date on the students table  
+CREATE TRIGGER `enrolment_date_student_table`
+AFTER INSERT ON enrolment
+FOR EACH ROW
+UPDATE student
+SET Enrolment_Date = NEW.Enrolment_Date
+WHERE Student_ID = NEW.Student_ID;
+
 -- Inserts department table with new head professor
 CREATE TRIGGER `insert_head_professor`
 AFTER INSERT ON departmenthead
